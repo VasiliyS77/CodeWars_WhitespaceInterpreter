42dbe1c43c0a615e0d5271d09dec9209f481e838
Vasiliy Shibayev <v_shibayev@mail.ru>
2022-10-15 19:05:01 +0600

    Базовая функция интерпретатора

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# Для более удобной отладки
+def unbleach(n: str) -> str:
+    """Преобразует строку языка Whitespace в последовательность вида sstntn"""
+    return n.replace(' ', 's').replace('\t', 't').replace('\n', 'n')
 
+
+# Whitespace Interpreter
+def whitecpace(code: str, inp :str = '') -> str:
+    output = ''
+    stack = []
+    heap = {}
+
+
+print("***", unbleach("  \t\n\t\t  "), "***")
